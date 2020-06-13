898183a32e0576eb1e82dbc099ee9077851d3036
Alexander Principle <avprinciple@gmail.com>
2020-06-13 14:26:08 +0300

    Fixed title header menu

--- _start-theme/inc/nav_menu.php
+++ _start-theme/inc/nav_menu.php
@@ -5,10 +5,11 @@ function theme_register_nav_menu() {
     'header_menu' => 'Главное меню',
   ) );
 }
+add_action( 'after_setup_theme', 'theme_register_nav_menu' );
 
 // Filters for header menu
-add_filter( 'wp_nav_menu_args', 'filter_wp_menu_args' );
-function filter_wp_menu_args( $args ) {
+add_filter( 'wp_nav_menu_args', 'filter_wp_nav_menu_args' );
+function filter_wp_nav_menu_args( $args ) {
   if ( $args['theme_location'] === 'header_menu' ) {
     $args['container']  = false;
     $args['menu_class'] = 'menu__list';
@@ -17,13 +18,13 @@ function filter_wp_menu_args( $args ) {
   return $args;
 }
 
-add_filter( 'nav_menu_item_id', 'filter_menu_item_css_id', 10, 4 );
-function filter_menu_item_css_id( $menu_id, $item, $args, $depth ) {
+add_filter( 'nav_menu_item_id', 'filter_nav_menu_item_css_id', 10, 4 );
+function filter_nav_menu_item_css_id( $menu_id, $item, $args, $depth ) {
   return $args->theme_location === 'header_menu' ? '' : $menu_id;
 }
 
-add_filter( 'nav_menu_css_class', 'filter_menu_item_css_class', 10, 4 );
-function filter_menu_item_css_class( $classes, $item, $args, $depth ) {
+add_filter( 'nav_menu_css_class', 'filter_nav_menu_item_css_class', 10, 4 );
+function filter_nav_menu_item_css_class( $classes, $item, $args, $depth ) {
   if ( $args->theme_location === 'header_menu' ) {
     $classes = [
       'menu__item'
@@ -34,7 +35,7 @@ function filter_menu_item_css_class( $classes, $item, $args, $depth ) {
 
 add_filter( 'nav_menu_link_attributes', 'filter_nav_menu_link_attributes', 10, 4 );
 function filter_nav_menu_link_attributes( $atts, $item, $args, $depth ) {
-  if ( $args->theme_location === 'header-menu' ) {
+  if ( $args->theme_location === 'header_menu' ) {
     $atts['class'] = 'menu__link';
     if ( $item->current ) {
       $atts['class'] .= ' menu__link--active';
@@ -46,7 +47,7 @@ function filter_nav_menu_link_attributes( $atts, $item, $args, $depth ) {
 // // Изменяет класс у вложенного ul
 // add_filter( 'nav_menu_submenu_css_class', 'filter_nav_menu_submenu_css_class', 10, 3 );
 // function filter_nav_menu_submenu_css_class( $classes, $args, $depth ) {
-//   if ( $args->theme_location === 'header-menu' ) {
+//   if ( $args->theme_location === 'header_menu' ) {
 //     $classes = [
 //       'menu__sub-list'
 //     ];
