cf86a5ddc7481fdc4eb067e41bf9053c3576dab4
Alexander Principle <avprinciple@gmail.com>
2020-06-13 14:24:53 +0300

    Removed meta fot title

--- _start-theme/header.php
+++ _start-theme/header.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-  <title><?= wp_get_document_title(); ?></title>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
   <?php wp_head(); ?>
